3f7fdf3e9125ff6dac1f07f6714820ee8493698a
Shreyas Srikanth <sshreyas567@gmail.com>
2024-02-07 15:11:27 +0530

    cart functionality

--- ecommerce-using-mongodb/app.js
+++ ecommerce-using-mongodb/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('65c340f2f21bc0941e19df51')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
