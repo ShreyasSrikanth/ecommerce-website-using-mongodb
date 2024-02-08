d2da224fdc9e576ca7482397a4636f0eb59ebf7d
Shreyas Srikanth <sshreyas567@gmail.com>
2024-02-08 07:52:00 +0530

    post product to cart working

--- ecommerce-using-mongodb/app.js
+++ ecommerce-using-mongodb/app.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const mongoConnect  = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -19,15 +18,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('65c340f2f21bc0941e19df51')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-  // next(); // Don't forget to call next() to continue to the next middleware/route handler
-});
+// app.use((req, res, next) => {
+//   User.findById('65c340f2f21bc0941e19df51')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+//   // next(); // Don't forget to call next() to continue to the next middleware/route handler
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +35,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://sshreyas567:7YuDDHSp3zU4xvmt@cluster0.siwts2j.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://sshreyas567:7YuDDHSp3zU4xvmt@cluster0.siwts2j.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
     app.listen(3000);
