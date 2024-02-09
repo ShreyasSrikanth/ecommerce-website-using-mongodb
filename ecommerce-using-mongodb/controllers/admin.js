146621fa23c4f60827203d2d72356654f1bdb91d
Shreyas Srikanth <sshreyas567@gmail.com>
2024-02-09 14:54:18 +0530

    relations formed between user and products

--- ecommerce-using-mongodb/controllers/admin.js
+++ ecommerce-using-mongodb/controllers/admin.js
@@ -19,7 +19,8 @@ exports.postAddProduct = (req, res, next) => {
     title:title,
     price:price,
     description:description,
-    imageUrl:imageUrl 
+    imageUrl:imageUrl,
+    userId: req.user
   });
     product
     .save()         // coming from mongoose
@@ -76,7 +77,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id')  //fetch related data
+    // .populate('userId', 'name')  //populate related fields
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
