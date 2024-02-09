8b2999afcbd4b5f58d63eeff582d00fe24d646fc
Shreyas Srikanth <sshreyas567@gmail.com>
2024-02-09 14:41:48 +0530

    user model setup

--- ecommerce-using-mongodb/app.js
+++ ecommerce-using-mongodb/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,15 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('65c340f2f21bc0941e19df51')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-//   // next(); // Don't forget to call next() to continue to the next middleware/route handler
-// });
+app.use((req, res, next) => {
+  User.findById('65c5ec18f9aef282200619ed')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +37,18 @@ mongoose
     'mongodb+srv://sshreyas567:7YuDDHSp3zU4xvmt@cluster0.siwts2j.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: 'Shreyas',
+          email: ' sshreyas567@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err=>{
